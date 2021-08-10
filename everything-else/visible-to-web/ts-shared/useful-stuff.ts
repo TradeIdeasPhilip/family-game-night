af1b2fad7a993ae6f8a171aad261f311fbca3f71
Philip Smolen <philip+git@trade-ideas.com>
2021-08-10 16:55:18 -0700

    Remove obsolete code.

--- everything-else/visible-to-web/ts-shared/useful-stuff.ts
+++ everything-else/visible-to-web/ts-shared/useful-stuff.ts
@@ -1,12 +1,3 @@
-// https://codereview.stackexchange.com/a/192241
-export function rot13(str: string) {
-  return str.replace(/[a-zA-Z]/gi, function (s) {
-    return String.fromCharCode(
-      s.charCodeAt(0) + (s.toLowerCase() < "n" ? 13 : -13)
-    );
-  });
-}
-
 // https://stackoverflow.com/a/39914235/971955
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
